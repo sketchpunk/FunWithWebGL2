07c7d8c71e832d9267e90596a5d3fed95452f5e4
Vor <sketchpunk@gmail.com>
2017-10-27 10:19:41 -0400

    051 - Transform Manipulator with Ray Intersection

---
+++ lesson_051/README.md
@@ -0,0 +1,12 @@
+# Fun with WebGL 2.0 - 051 - Transform Manipulator with Ray Intersection
+**Description**:
+Bonus video related to ray intersections. This time we take an old example of how to build a manipulator ui control but instead of using picking to tell that you clicked on it, we will use rays and a bit of math to determine which axis OR plane you want to move your object. We build the UI tool further where we can attach a renderable to it and then move it around our scene.
+
+### Links of Interest
+https://www.youtube.com/watch?v=Rw7KjHLFV4g
+
+### Links
+* [Lesson on Youtube](https://youtu.be/B-6SpH3oXhM)
+* [Youtube Series PlayList](https://www.youtube.com/playlist?list=PLMinhigDWz6emRKVkVIEAaePW7vtIkaIF)
+* [Fungi Git Change 1](https://github.com/sketchpunk/FunWithWebGL2/commit/5243b27f6d9a8ca2a618308c55dda9c0f9889b4f)
+* [Fungi Git Change 2](https://github.com/sketchpunk/FunWithWebGL2/commit/fe57893b8204fd9c22acd4f62d2601b8897cc599)
---
+++ lesson_051/fungi/primitives/FacedCube.js
@@ -0,0 +1,73 @@
+import gl,{ VAO }	from "../gl.js";
+import Renderable	from "../entities/Renderable.js";
+
+function FacedCube(matName){
+	//If the vao exists, create a new renderable using it.
+	if(gl.res.vao["FungiFCube"]) return new Renderable(gl.res.vao["FungiFCube"],matName);
+
+
+	var width = 1, height = 1, depth = 1, x = 0, y = 0, z = 0;
+	var w = width*0.5, h = height*0.5, d = depth*0.5;
+	var x0 = x-w, x1 = x+w, y0 = y-h, y1 = y+h, z0 = z-d, z1 = z+d;
+
+	//Starting bottom left corner, then working counter clockwise to create the front face.
+	//Backface is the first face but in reverse (3,2,1,0)
+	//keep each quad face built the same way to make index and uv easier to assign
+	var aVert = [
+		x0, y1, z1, 0,	//0 Front
+		x0, y0, z1, 0,	//1
+		x1, y0, z1, 0,	//2
+		x1, y1, z1, 0,	//3 
+
+		x1, y1, z0, 1,	//4 Back
+		x1, y0, z0, 1,	//5
+		x0, y0, z0, 1,	//6
+		x0, y1, z0, 1,	//7 
+
+		x1, y1, z1, 2,	//3 Right
+		x1, y0, z1, 2,	//2 
+		x1, y0, z0, 2,	//5
+		x1, y1, z0, 2,	//4
+
+		x0, y0, z1, 3,	//1 Bottom
+		x0, y0, z0, 3,	//6
+		x1, y0, z0, 3,	//5
+		x1, y0, z1, 3,	//2
+
+		x0, y1, z0, 4,	//7 Left
+		x0, y0, z0, 4,	//6
+		x0, y0, z1, 4,	//1
+		x0, y1, z1, 4,	//0
+
+		x0, y1, z0, 5,	//7 Top
+		x0, y1, z1, 5,	//0
+		x1, y1, z1, 5,	//3
+		x1, y1, z0, 5	//4
+	];
+
+	//Build the index of each quad [0,1,2, 2,3,0]
+	var aIndex = [];
+	for(var i=0; i < aVert.length / 4; i+=2) aIndex.push(i, i+1, (Math.floor(i/4)*4)+((i+2)%4));
+
+	//Build UV data for each vertex
+	var aUV = [];
+	for(var i=0; i < 6; i++) aUV.push(0,0,	0,1,  1,1,  1,0);
+
+	//Build Normal data for each vertex
+	var aNorm = [
+		 0, 0, 1,	 0, 0, 1,	 0, 0, 1,	 0, 0, 1,		//Front
+		 0, 0,-1,	 0, 0,-1,	 0, 0,-1,	 0, 0,-1,		//Back
+		-1, 0, 0,	-1, 0, 0,	-1, 0, 0,	-1, 0, 0,		//Left
+		 0,-1, 0,	 0,-1, 0,	 0,-1, 0,	 0,-1, 0,		//Bottom
+		 1, 0, 0,	 1, 0, 0,	 1, 0, 0,	 1, 0, 0,		//Right
+		 0, 1, 0,	 0, 1, 0,	 0, 1, 0,	 0, 1, 0		//Top
+	];
+
+	var vao = VAO.standardRenderable("FungiFCube",4,aVert,aNorm,aUV,aIndex),
+		entity = new Renderable(vao,matName);
+	
+	entity.name = "facedCube";
+	return entity;
+}
+
+export default FacedCube;
